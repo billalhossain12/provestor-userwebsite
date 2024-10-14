def4e7f0699c087c42e2a53af8a2fcc65ff4e560
billalhossain12 <billalhossain343434@gmail.com>
2024-10-14 11:53:35 +0600

    added study page with responsive styles

--- src/app/invest/page.tsx
+++ src/app/invest/page.tsx
@@ -13,71 +13,170 @@ import "swiper/css/navigation";
 // import "./PropertySlider.module.css";
 import PropertyCard from "@/components/UI/PropertyCard";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useState } from "react";
+import { Accordion, AccordionBody, AccordionHeader } from "@material-tailwind/react";
+
+
+export function MaterialIcon({ id, open }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
 
 export default function InvestPage() {
+  const [isVisible, setIsVisible] = useState(false);
+  const handleFilterToggle = () => {
+    setIsVisible(!isVisible);
+  };
+
+
+
+  const [openPropertyType, setOpenPropertyType] = useState(0);
+  const handleOpenPropertyType = (value) =>
+    setOpenPropertyType(openPropertyType === value ? 0 : value);
+
+  const handleChangePropertyType = (e) => {
+    const selectedText = e.target.nextSibling.innerText;
+  };
   return (
-    <section className="md:px-[4rem] px-[0.3rem] md:mb-[7.5rem] mb-[4rem]">
-      <div className="md:py-[3rem] py-[1rem]">
-        <div className="flex md:flex-row flex-col justify-between items-center">
-          <h3 className="lg:text-[3rem] md:text-[2.5rem] text-[2rem] font-semibold leading-[2.37rem]">
-            All Properties
+    <>
+
+
+
+      <section className="lg:px-[5.5rem] md:px-[1rem] px-[0.5rem] pb-[5rem] relative overflow-hidden min-h-[1050px]">
+        {/* Filter Card  */}
+        <div
+      className={`${
+        isVisible ? "right-0" : "right-[-120rem]"
+      } duration-300 absolute  right-0 md:min-w-[350px] min-w-[250px] min-h-full md:p-[3rem] p-[1rem] bg-white shadow-2xl z-[10]`}
+    >
+      <div className="flex items-center justify-between">
+        <h3 className="text-[2rem] font-semibold">Filters</h3>
+        <Icon
+          onClick={handleFilterToggle}
+          className="w-[2.5rem] h-[2.5rem] cursor-pointer overflow-hidden text-red-600"
+          icon="ph:x-square-thin"
+        />
+      </div>
+
+
+      {/* Property Types UI  */}
+      <div className="pt-[1.8rem]">
+      <Accordion
+        className="border-b-[1px] border-b-[#D3D3D3]"
+        open={openPropertyType === 1}
+        icon={<MaterialIcon id={1} open={openPropertyType} />}
+      >
+        <AccordionHeader
+          className="border-b border-transparent"
+          onClick={() => handleOpenPropertyType(1)}
+        >
+          <h3 className="text-[1.25rem] !font-semibold font-openSans text-[#353535] leading-[1.25rem]">
+            Property Type
           </h3>
-          <button
-            // onClick={handleFilterToggle}
-            className="bg-[#24B24B] hover:bg-[#000] hover:text-[#fff] duration-300 px-[3rem] py-[1rem] font-semibold flex items-center gap-3 md:mt-0 mt-[1rem]"
-          >
-            <span>Filters</span>
-            <Icon className="text-2xl" icon="mdi:filter" />
-          </button>
-        </div>
-        {/* <FilterToasts /> */}
-      </div>
+        </AccordionHeader>
 
-      <div className="md:mt-[3.68rem] mt-[1rem]">
+        <AccordionBody>
+          <div className="space-y-[1rem] font-openSans">
+            {[]?.map((propertyType, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <input
+                  className="w-[1.1rem] h-[1.1rem] propertyTypes"
+                  id={propertyType}
+                  type="checkbox"
+                  onChange={handleChangePropertyType}
+                />
+                <label
+                  htmlFor={propertyType}
+                  className="text-[1.25rem] leading-[2rem] select-none"
+                >
+                  {propertyType}
+                </label>
+              </div>
+            ))}
+          </div>
+        </AccordionBody>
+      </Accordion>
+    </div>
+    
+
+    </div>
+        
+
+        <div className="md:py-[3rem] py-[1rem]">
+          <div className="flex md:flex-row flex-col justify-between items-center">
+            <h3 className="lg:text-[3rem] md:text-[2.5rem] text-[2rem] font-semibold leading-[2.37rem]">
+              All Properties
+            </h3>
+            <button
+              onClick={handleFilterToggle}
+              className="bg-[#24B24B] hover:bg-[#000] hover:text-[#fff] duration-300 px-[3rem] py-[1rem] font-semibold flex items-center gap-3 md:mt-0 mt-[1rem]"
+            >
+              <span>Filters</span>
+              <Icon className="text-2xl" icon="mdi:filter" />
+            </button>
+          </div>
+        </div>
+        {/* Property list container  */}
         <div>
-          <Swiper
-            slidesPerView={3}
-            navigation={true}
-            breakpoints={{
-              200: {
-                slidesPerView: 1,
-                spaceBetween: 40,
-              },
+            <Swiper
+              slidesPerView={3}
+              navigation={true}
+              breakpoints={{
+                200: {
+                  slidesPerView: 1,
+                  spaceBetween: 40,
+                },
 
-              900: {
-                slidesPerView: 2,
-                spaceBetween: 40,
-              },
+                900: {
+                  slidesPerView: 2,
+                  spaceBetween: 40,
+                },
 
-              1000: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-              },
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination, Navigation, Autoplay]}
-            className="mySwiper"
-          >
-            <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
-              <PropertyCard />
-            </SwiperSlide>
-            <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
-              <PropertyCard />
-            </SwiperSlide>
-            <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
-              <PropertyCard />
-            </SwiperSlide>
-            <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
-              <PropertyCard />
-            </SwiperSlide>
-            <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
-              <PropertyCard />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
-    </section>
+                1000: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination, Navigation, Autoplay]}
+              className="mySwiper"
+            >
+              <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
+                <PropertyCard />
+              </SwiperSlide>
+              <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
+                <PropertyCard />
+              </SwiperSlide>
+              <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
+                <PropertyCard />
+              </SwiperSlide>
+              <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
+                <PropertyCard />
+              </SwiperSlide>
+              <SwiperSlide className="md:mb-[3rem] mb-[1.5rem]">
+                <PropertyCard />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+      </section>
+    </>
   );
 }
---
+++ src/app/study/page.tsx
@@ -0,0 +1,145 @@
+"use client"
+import Image from "next/image";
+import studyBannerImg from "../../assets/study-banner.svg";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import "./StudyCenterSlider.css";
+
+// import required modules
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+const studyCenterDataArray = [
+  {
+    title: "How to become real estate agent",
+    content:
+      "This involves finding a brokerage that aligns with your goals, values, and business philosophy and entering into an agreement to work under their supervision.",
+    link: "https://www.youtube.com/embed/W5jyH4j_Yps?si=sGO1QBT6P8PJ0DHh",
+  },
+  {
+    title: "Qualification for real",
+    content: "Around 60 to 180 hours, depending on the state.",
+    link: "https://www.youtube.com/embed/D69VhdRNok0?si=2rHlzmAs6XEX7bWQ",
+  },
+  {
+    title: "Financial investments to start",
+    content:
+      "Which can range from a few hundred to a few thousand dollars, depending on the provider and state requirements.",
+    link: "https://www.youtube.com/embed/UJv9-F7SN5A?si=moGafn_7LdWGFoJE",
+  },
+  {
+    title: "Resources for new agents",
+    content:
+      "Training courses cover a wide range of topics, from sales and marketing to contract negotiation.",
+    link: "https://www.youtube.com/embed/yL0zzGsM1qg?si=fC4i2R54WafRqUAa",
+  },
+];
+
+export default function StudyPage() {
+  return (
+    <main className="bg-[#F7F4F2] pb-[0.5rem]">
+      {/* Banner  */}
+      <section className="lg:px-[4rem] px-[0.5rem] py-[4rem]">
+        <div className="bg-[#FFF] mx-auto grid lg:grid-cols-2 grid-cols-1 lg:px-[3rem] lg:py-[5.5rem] py-[1rem] gap-[3rem]  px-[1rem]">
+          <div className="flex items-center">
+            <div className="lg:mt-[-13px] mt-[3rem]">
+              <h1 className="lg:text-[2.5rem] md:text-[2rem] text-[1.5rem] font-bold md:leading-[3.5rem] leading-[2rem] relative">
+                <span className="absolute left-[-0.5rem] top-[0.6rem] bg-[#24B24B] h-[2rem] w-[3px]"></span>{" "}
+                <span>Explore and Expand Your</span>
+                <br />
+                <span className="text-[#24B24B]">Real Estate Expertise.</span>
+              </h1>
+              <p className="md:text-[1.5rem] text-[1rem] font-semibold md:leading-[2.1rem] leading-[2rem] mt-[1.5rem]">
+                Elevate your real estate journey with our expert insights,
+                fostering continuous learning for property investment
+                excellence.
+              </p>
+            </div>
+          </div>
+          <div>
+            <Image
+              className="lg:max-w-[600px] w-full"
+              src={studyBannerImg}
+              alt="hero-banner"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Cards  */}
+      <section className="mb-[4rem] lg:mx-[4rem] mx-[0.5rem] bg-[#FFF] md:p-[3rem] ">
+        <h3 className="md:text-[2rem] text-[1.5rem] font-semibold leading-[2.5rem] border-b-[1.5px] border-gray-500 inline-block mb-[3rem]">
+          Study Center
+        </h3>
+
+        <>
+          <Swiper
+            slidesPerView={3}
+            navigation={true}
+            breakpoints={{
+              200: {
+                slidesPerView: 1,
+                spaceBetween: 40,
+              },
+
+              900: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+              },
+
+              1000: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination, Navigation, Autoplay]}
+            className="mySwiper px-[5rem] mb-[3rem]"
+          >
+            <div>
+              {studyCenterDataArray?.map(({title, link, content}, index) => (
+                <SwiperSlide key={index} className="mb-[3rem]">
+                  <div
+                    className="bg-[#fff] rounded-[0.125rem] p-[0.5rem] h-[450px]"
+                    style={{ boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.10)" }}
+                  >
+                    <div className="">
+                      <iframe
+                        className="w-full h-[200px]"
+                        src={link}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                      ></iframe>
+                    </div>
+
+                    <div className="p-[1rem] space-y-3">
+                      <h3 className="text-[1.5rem] font-semibold leading-6">
+                        {title}
+                      </h3>
+                      <p>{content}</p>
+                      <div className="flex items-center gap-[0.5rem] text-[#B1B1B1]">
+                        <Icon icon="mdi:clock" />
+                        <p>30 Mins</p>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </div>
+          </Swiper>
+        </>
+      </section>
+    </main>
+  );
+}
